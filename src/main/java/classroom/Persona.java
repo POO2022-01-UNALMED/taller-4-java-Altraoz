a69d4dd20e9de497eeb723e5e60b2087a5d8aef6
Altraoz <curresty@unal.edu.co>
2022-04-05 22:38:39 -0500

    Update Persona.java

--- src/main/java/classroom/Persona.java
+++ src/main/java/classroom/Persona.java
@@ -2,13 +2,12 @@ package classroom;
 
 public class Persona {
 
-    final long cedula;
+    long cedula;
     String nombre;
     static int totalPersonas;
-    
+
     static {
         totalPersonas = 0;
-        cedula = 3;
     }
 
     Persona(long cedula, String nombre) {
@@ -16,7 +15,7 @@ public class Persona {
         this.nombre = nombre;
         totalPersonas++;
     }
-    
+
     Persona(String nombre, long cedula) {
         this.cedula = cedula;
         this.nombre = nombre;
@@ -33,7 +32,7 @@ public class Persona {
         this.nombre = "";
         totalPersonas++;
     }
-    
+
     long getCedula() {
         return cedula;
     }
